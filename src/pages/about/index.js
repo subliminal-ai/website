9ec2995632431330b48190d57fc81fe7c33143ef
Kabir Khan <kabir@subliminal.ai>
2020-03-30 15:35:20 -0700

    fix about page text

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -22,21 +22,21 @@ const About = ({ data, image, kabir_image }) => {
       />
       <div style={{width: '100%', marginTop: 30}}>
         <Row>
-          <Col xs={{span: 24}} sm={{span: 18, offset: 2}} md={{span: 12, offset: 6}}>
-            <h1 style={{textAlign: 'center', fontSize: '3em'}}>
+          <Col xs={{span: 22, offset: 1}} sm={{span: 18, offset: 2}} md={{span: 12, offset: 6}}>
+            <h2 style={{textAlign: 'center'}}>
               What we believe
-            </h1>
-            <p style={{textAlign: 'center', fontSize: '1.7em'}}>
+            </h2>
+            <p>
               At Subliminal AI, we have a set of core beliefs that drive every one of our projects.
             </p>
           </Col>
         </Row>
         <Row style={{marginTop: 50}}>
           <Col xs={{span: 24}} sm={{span: 18, offset: 2}} md={{span:12, offset: 6}}>
-            <h2 style={{fontSize: '2em', color: '#444'}}>
+            <h3>
               <strong>1. Augmentation {'>'} Automation</strong>
-            </h2>
-            <p style={{fontSize: '1.7em', marginLeft: '1em'}}>
+            </h3>
+            <p style={{marginLeft: '1em'}}>
               At Subliminal AI we believe in <strong>Augementation</strong> over <strong>Automation.</strong>&nbsp;
               We are <strong>not</strong> in the business of replacing your workforce with fancy AI models. We <strong>are</strong> in the business of augmenting your company's main workflows to help make each one of your employees more efficient and effective at their job.
             </p>
@@ -44,20 +44,20 @@ const About = ({ data, image, kabir_image }) => {
         </Row>
         <Row style={{marginTop: 50}}>
           <Col xs={{span: 24}} sm={{span: 18, offset: 2}} md={{span:12, offset: 6}}>
-            <h2 style={{fontSize: '2em', color: '#444'}}>
+            <h3>
               <strong>2. Artificial Intelligence is not Magic</strong>
-            </h2>
-            <p style={{fontSize: '1.7em', marginLeft: '1em'}}>
+            </h3>
+            <p style={{marginLeft: '1em'}}>
               AI is a relatively new field. It has incredible potential but it's only useful if it's easy to understand. We strive to build explainable, actionable and reliable AI products, not moonshots.
             </p>
           </Col>
         </Row>
         <Row style={{marginTop: 50}}>
           <Col xs={{span: 24}} sm={{span: 18, offset: 2}} md={{span:12, offset: 6}}>
-            <h2 style={{fontSize: '2em', color: '#444'}}>
+            <h3>
               <strong>3. Quality > Speed</strong>
-            </h2>
-            <p style={{fontSize: '1.7em', marginLeft: '1em'}}>
+            </h3>
+            <p style={{marginLeft: '1em'}}>
               Our priority is always to build <strong>high-quality</strong> AI solutions, not to get your project done in 2 weeks.
             </p>
           </Col>
