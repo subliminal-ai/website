a84a88c970171193b9a97986f1ad083d7b288406
Kabir Khan <kabir@subliminal.ai>
2020-03-30 15:20:20 -0700

    Fixing Contact form submission

--- src/components/letsTalk/index.js
+++ src/components/letsTalk/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Row, Col, Form, Input, Button, Checkbox } from 'antd';
+import { Row, Col, Form, Input, Result } from 'antd';
 
 import { 
   Section, Title, SubTitle, Card, FormTitle, 
@@ -27,24 +27,13 @@ const tailLayout = {
 
 const LetsTalk = () => {
 
-  const [fields, setFields] = useState([
-    {
-      name: ['name'],
-      value: null,
-    },
-    {
-      name: ['company_name'],
-      value: null,
-    },
-    {
-      name: ['email'],
-      value: null,
-    },
-    {
-      name: ['project_description'],
-      value: null,
-    },
-  ]);
+  const FormStatus = Object.freeze({
+      SUCCESS: "SUCCESS",
+      ERROR: "ERROR",
+      NOT_SUBMITTED: "NOT_SUBMITTED"
+  });
+
+  const [formStatus, setFormStatus] = useState(FormStatus.NOT_SUBMITTED);
 
   const encode = (data) => {
     return Object.keys(data)
@@ -52,22 +41,83 @@ const LetsTalk = () => {
         .join("&");
   }
 
-  const handleSubmit = (values, more) => {
-    console.log("FORM SUBMITTED")
-    console.log(values, more)
-    // fetch("/", {
-    //   method: "POST",
-    //   headers: { "Content-Type": "application/x-www-form-urlencoded" },
-    //   body: encode({ "form-name": "contact", ...values })
-    // })
-    //   .then((res) => console.log("Success!", res))
-    //   .catch(error => alert(error));
+  const handleSubmit = (values) => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...values })
+    })
+      .then(() => setFormStatus(FormStatus.SUCCESS))
+      .catch(error => setFormStatus(FormStatus.ERROR));
   };
 
-  const onFinishFailed = errorInfo => {
-    console.log('Failed:', errorInfo);
-  };
-
+  const renderFormStatus = (_formStatus) => {
+    console.log("FORM STATUS", _formStatus)
+    switch(_formStatus) {
+      case FormStatus.NOT_SUBMITTED:
+        return (
+          <Form
+            layout="vertical"
+            size="large"
+            name="contact"
+            onFinish={handleSubmit}
+            // onFinishFailed={onFinishFailed}
+            scrollToFirstError>
+            <Form.Item 
+              name="name"
+              label="Full Name"
+              rules={[{ required: true, message: 'Please input your Full Name' }]}>
+              <StyledInput placeholder="Enter your full name..."/>
+            </Form.Item>
+            <Form.Item
+              name="company_name"
+              label="Company Name"
+              rules={[{ required: true, message: 'Please input your Company or Organization Name' }]}>
+              <StyledInput placeholder="Enter the name of your company..." />
+            </Form.Item>
+            <Form.Item 
+              name="email"
+              label="Email Address"
+              rules={[
+                { type: "email", message: 'Please provide a valid email address'},
+                { required: true, message: 'Please input your email' }
+              ]}>
+              <StyledInput placeholder="Enter your email..." />
+            </Form.Item>
+            <Form.Item 
+              name="project_description"
+              label="Project Description"
+              rules={[
+                { required: true, message: 'Please provide a description of your project or idea' },
+                { min: 30, message: 'Please provide some more detail about your project.' },
+              ]}>
+              <StyledTextArea 
+                placeholder="Provide a description of your project idea."
+                rows={6} />
+            </Form.Item>
+            <Form.Item>
+              <SubmitButton type="primary" htmlType="submit" size="large">Submit</SubmitButton>
+            </Form.Item>
+          </Form>
+        )
+      case FormStatus.SUCCESS:
+        return (
+          <Result
+            status="success"
+            title="Successfully submitted form!"
+            subTitle="Thank you for your interest. We'll get back to you as soon as we can."
+          />
+        )
+      case FormStatus.ERROR:
+        return (
+          <Result
+            status="error"
+            title="Error submitting form!"
+            subTitle="This form seems to be broken right now. Please schedule a meeting with the Schedule a Call button to the left."
+          />
+        )
+    }
+  }
 
   return (
     <Section>
@@ -92,50 +142,8 @@ const LetsTalk = () => {
         </Col>
         <Col xs={24} sm={12}>
           <Card>
-            <Form
-              layout="vertical"
-              size="large"
-              name="contact"
-              onFinish={handleSubmit}
-              onFinishFailed={onFinishFailed}
-              scrollToFirstError>
-              <FormTitle>Let’s talk!</FormTitle>
-              <Form.Item 
-                name="name"
-                label="Full Name"
-                rules={[{ required: true, message: 'Please input your Full Name' }]}>
-                <StyledInput placeholder="Enter your full name..."/>
-              </Form.Item>
-              <Form.Item
-                name="company_name"
-                label="Company Name"
-                rules={[{ required: true, message: 'Please input your Company or Organization Name' }]}>
-                <StyledInput placeholder="Enter the name of your company..." />
-              </Form.Item>
-              <Form.Item 
-                name="email"
-                label="Email Address"
-                rules={[
-                  { email: true, message: 'Please provide a valid email address'},
-                  { required: true, message: 'Please input your email' }
-                ]}>
-                <StyledInput placeholder="Enter your email..." />
-              </Form.Item>
-              <Form.Item 
-                name="project_description"
-                label="Project Description"
-                rules={[
-                  { required: true, message: 'Please provide a description of your project or idea' },
-                  { min: 30, message: 'Please provide some more detail about your project.' },
-                ]}>
-                <StyledTextArea 
-                  placeholder="Provide a description of your project idea."
-                  rows={6} />
-              </Form.Item>
-              <Form.Item>
-                <SubmitButton type="primary" htmlType="submit" size="large">Submit</SubmitButton>
-              </Form.Item>
-            </Form>
+            <FormTitle>Let’s talk!</FormTitle>
+            {renderFormStatus(formStatus)}
           </Card>
         </Col>
       </Row>
